e5adae736ff36642afb1fc64aae5518b03b17f33
Andrew Wolf <wolfsolutions7@gmail.com>
2021-01-13 07:36:34 -0500

    adding q2 printing first 100 odd numbers

---
+++ q2_print_odd_numbers.py
@@ -0,0 +1,11 @@
+"""  The purpose of this file is to print the first 100 odd numbers """
+
+# Requirements: "Print first 100 odd numbers in Python."
+
+i = 1
+x = 1
+
+while i <= 100:
+    print(x)
+    x += 2
+    i += 1
