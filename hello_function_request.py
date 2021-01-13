2aaeb1cf66e84a1fd699dcb7b851572808d78c2f
Andrew Wolf <wolfsolutions7@gmail.com>
2021-01-13 07:16:49 -0500

    q1 hello world

---
+++ hello_function_request.py
@@ -0,0 +1,13 @@
+# The purpose of this file is to answer the questions provided by coding tmeple leadership.
+
+# Question 1
+# Write a function to print "hello_USERNAME!" USERNAME is the input of the function. The first line of the code has been defined as below.
+# def hello_name(user_name):
+#         .....
+
+
+def hello_function(user_name):
+    print(f"hello_{user_name}!")
+
+
+hello_function('Nicole')
